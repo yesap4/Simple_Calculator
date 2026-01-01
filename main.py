547a1effa12a432528ad27429a0aa54ea8d540cd
Anant Prakash <yes4ap@gmail.com>
2026-01-01 08:47:25 +0000

    Implement basic functionality for Simple Calculator with input handling and operation selection

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+# Import Module
+import streamlit as st
 
+# Page Header
+st.set_page_config('Simple Calculator')
+
+# Page Title
+
+st.title('Simple Calculator')
+
+# Description
+st.info('Enter Your First Number and Second Number, add select an operation. Get The Results')
+
+
+# Taking Input
+
+first_num = st.number_input('Enter Your First Number: ',min_value=0)
+
+sec_num = st.number_input('Enter Your Second Number: ',min_value=0)
+
+# Operation Option
+
+operation = st.selectbox('Select An Operation',['+','-','*','/'])
+
+# Checking Which operation is selected and what happens
+
+if operation == '+':
+    result = first_num + sec_num
+    st.success(f'The Sum of {first_num} and {sec_num} is = {result}')
+
+elif operation == '-':
+    result = sec_num - first_num
+    st.success(f'The difference between {sec_num} and {first_num} is  = {result} ')    
+elif operation == '*':
+    result = first_num * sec_num
+    st.success(f'The Product of {first_num} and {sec_num} is = {result}')
+elif operation == '/': # The result is in decimal to avoid it we use floor division (//)
+    result = first_num // sec_num
+    st.success(f'The Quotient = {result}')
+
+
+# Thanks 
